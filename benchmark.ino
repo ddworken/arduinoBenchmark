3f4e917e3ea8065d1b1ffac57a24bd9126fb58c6
ddworken <ddworken@gmail.com>
2014-06-29 00:27:54 -0400

    Added more tests
    Added integer: multiplication and division and floating point: addition/subtraction. 
    Refactored code to increase compiler optimization while still preventing premature optimization of calculations.

--- benchmark.ino
+++ benchmark.ino
@@ -21,6 +21,12 @@ void loop(){
 	dot(1);
 	int floatingPointDivisionSpeed = testFloatingPointDivision();
 	dot(1);
+	int integerMultiplicationSpeed = testIntegerMultiplication();
+	dot(1);
+	int integerDivisionSpeed = testIntegerDivision();
+	dot(1);
+	int floatingPointAdditionSubtractionSpeed = testFloatingPointAdditionSubtraction();
+	dot(1);
 	Serial.println(" ");
 	Serial.print("You scored a ");
 	Serial.print(eepromReadSpeed);
@@ -31,6 +37,15 @@ void loop(){
 	Serial.print("You scored a ");
 	Serial.print(integerAdditionSubtractionSpeed);
 	Serial.println(" on the integer addition subtraction test (lower is better).");
+	Serial.print("You scored a ");
+	Serial.print(integerMultiplicationSpeed);
+	Serial.println(" on the integer multiplication test (lower is better).");
+	Serial.print("You scored a ");
+	Serial.print(integerDivisionSpeed);
+	Serial.println(" on the integer division test (lower is better).");
+	Serial.print("You scored a ");
+	Serial.print(floatingPointAdditionSubtractionSpeed);
+	Serial.println(" on the floating point addition subtraction test (lower is better).");
 	Serial.print("You scored a ");
 	Serial.print(floatingPointMultiplicationSpeed);
 	Serial.println(" on the floating point multiplication test (lower is better).");
@@ -68,9 +83,9 @@ int testEepromRead(){
 }
 
 int testIntegerAdditionSubtraction(){
-	int val;
-	int volatile i;
-	int volatile k;
+	int volatile val;
+	int i;
+	int k;
 	int startVal = millis();
 	for(int j = 0; j < 10; j++){
 		for(i = 0; i < 10000; i++){
@@ -86,10 +101,65 @@ int testIntegerAdditionSubtraction(){
 	return endVal-startVal;
 }
 
+int testIntegerMultiplication(){
+	int volatile val;
+	int i;
+	int k;
+	int startVal = millis();
+	for(int j = 0; j < 10; j++){
+		for(i = 0; i < 10000; i++){
+			val = i *42;
+		}
+		for(k = 0; k < 10000; k++){
+			val = k * 42;
+		}
+		val = 0;
+	}
+	int endVal = millis();
+	return endVal-startVal;
+}
+
+int testIntegerDivision(){
+	int volatile val;
+	int i;
+	int k;
+	int startVal = millis();
+	for(int j = 0; j < 10; j++){
+		for(i = 0; i < 10000; i++){
+			val = i  / 42;
+		}
+		for(k = 0; k < 10000; k++){
+			val = k / 42;
+		}
+		val = 0;
+	}
+	int endVal = millis();
+	return endVal-startVal;
+}
+
+int testFloatingPointAdditionSubtraction(){
+	float volatile val = 42.4242;
+	int j;
+	int i;
+	int startVal = millis();
+	for(j = 0; j < 10; j++){
+		for(i = 0; i < 10000; i++){
+			val=val+i;
+			val=42.4242;
+		}
+		for(int k = 0; k < 10000; k++){
+			val=val-k;
+			val=42.4242;
+		}
+	}
+	int endVal = millis();
+	return endVal-startVal;
+}
+
 int testFloatingPointMultiplication(){
-	float val = 42.4242;
-	int volatile j;
-	int volatile i;
+	float volatile val = 42.4242;
+	int j;
+	int i;
 	int startVal = millis();
 	for(j = 0; j < 10; j++){
 		for(i = 0; i < 10000; i++){
@@ -103,9 +173,9 @@ int testFloatingPointMultiplication(){
 }
 
 int testFloatingPointDivision(){
-	float val = 42.4242;
-	int volatile j;
-	int volatile i;
+	float volatile val = 42.4242;
+	int j;
+	int i;
 	int startVal = millis();
 	for(j = 0; j < 10; j++){
 		for(i = 0; i < 10000; i++){
@@ -118,12 +188,9 @@ int testFloatingPointDivision(){
 	return endVal-startVal;
 }
 
-int ramSpeedTest(){
-	//String longString1 PROGMEM = "This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. This is a long string meant to use up a large amount of ram. ";
-}
-
 void dot(int j){
 	for(int i = 0; i < j; i++){
 		Serial.print(".");
 	}
 }
+
